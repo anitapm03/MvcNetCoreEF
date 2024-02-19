4609e49bbc909743304c68feac5089dc0f6e2e62
anitapm03 <apereiramolinero@gmail.com>
2024-02-19 12:33:36 +0100

    crud hecho

--- MvcNetCoreEF/Controllers/HospitalesController.cs
+++ MvcNetCoreEF/Controllers/HospitalesController.cs
@@ -18,5 +18,44 @@ namespace MvcNetCoreEF.Controllers
             List<HospitalEF> hospitales = this.repo.GetHospitales();
             return View(hospitales);
         }
+
+        public IActionResult Details(int idhospital)
+        {
+            HospitalEF hospital = this.repo.FindHospital(idhospital);
+            return View(hospital);
+        }
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(HospitalEF hospital)
+        {
+            this.repo.InsertarHospital(hospital.IdHospital, hospital.Nombre,
+                hospital.Direccion, hospital.Telefono, hospital.Camas);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Modificar(int idhospital)
+        {
+            HospitalEF hospital = this.repo.FindHospital(idhospital);
+            return View(hospital);
+        }
+
+        [HttpPost]
+        public IActionResult Modificar(HospitalEF hospital)
+        {
+            this.repo.ModificarHospital(hospital.IdHospital, hospital.Nombre,
+                hospital.Direccion, hospital.Telefono, hospital.Camas);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Eliminar(int idhospital)
+        {
+            this.repo.DeleteHospital(idhospital);
+            return RedirectToAction("Index");
+        }
     }
 }
--- MvcNetCoreEF/Repositories/RepositoryHospital.cs
+++ MvcNetCoreEF/Repositories/RepositoryHospital.cs
@@ -20,5 +20,63 @@ namespace MvcNetCoreEF.Repositories
                            select datos;
             return consulta.ToList();
         }
+
+        public HospitalEF FindHospital(int idHospital)
+        {
+            var consulta = from datos in this.context.Hospitales
+                           where datos.IdHospital == idHospital
+                           select datos;
+
+            //por si no encontramos el  por el id. Tenemos un metodo
+            //que es FirstOrDefault() que devuelve el primer registro
+            //el valor por defect del model
+            return consulta.FirstOrDefault();
+        }
+
+        public void InsertarHospital
+            (int idHospital, string nombre, string direccion,
+            string telefono, int camas)
+        {
+            //instanciar el obj
+            HospitalEF hospital = new HospitalEF();
+            hospital.IdHospital = idHospital;
+            hospital.Nombre = nombre;
+            hospital.Direccion = direccion;
+            hospital.Telefono = telefono;
+            hospital.Camas = camas;
+
+            //añadimos el model a la coleccion dbset
+            this.context.Hospitales.Add(hospital);
+            //almacenamos en la BBDD
+            this.context.SaveChanges();
+        }
+
+        public void ModificarHospital
+            (int idHospital, string nombre, string direccion,
+            string telefono, int camas)
+        {
+            //buscamos el hospital por su id
+            HospitalEF hospital = this.FindHospital(idHospital);
+
+            //modificamos sus propiedades
+            hospital.Nombre = nombre;
+            hospital.Direccion = direccion;
+            hospital.Telefono = telefono;
+            hospital.Camas = camas;
+
+            //guardamos los cambios en la bbdd
+            this.context.SaveChanges();
+
+        }
+
+        public void DeleteHospital(int idHospital)
+        {
+            //buscamos el model hospital para eliminarlo
+            HospitalEF hospital = this.FindHospital(idHospital);
+            //eliminamos el obj del dbset
+            this.context.Hospitales.Remove(hospital);
+            //guardamos los cambios en la bbdd
+            this.context.SaveChanges();
+        }
     }
 }
